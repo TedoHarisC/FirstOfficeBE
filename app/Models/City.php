1c1b1069c9880d2d1cde83503272723fb97337a5
TedoHarisC <tedoharischandra29@gmail.com>
2025-04-15 07:09:55 +0700

    Make ORM Relationship and Model

--- app/Models/City.php
+++ app/Models/City.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class City extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'photo',
+    ];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value; // tedo office center
+        $this->attributes['slug'] = Str::slug($value); // tedo-office-center
+    }
+
+    // Karena relasi nya one to many dengan office space (Maka nama prural officeSpaces)
+    public function officeSpaces(): HasMany
+    {
+        return $this->hasMany(OfficeSpace::class);
+    }
 }
--- app/Models/OfficeSpace.php
+++ app/Models/OfficeSpace.php
@@ -3,8 +3,49 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class OfficeSpace extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    // Fillable ini adalah data yang bisa diisi oleh user
+    protected $fillable = [
+        'name',
+        'thumbnail',
+        'is_open',
+        'is_full_booked',
+        'price',
+        'duration',
+        'address',
+        'about',
+        'slug',
+        'city_id'
+    ];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value; // tedo office center
+        $this->attributes['slug'] = Str::slug($value); // tedo-office-center
+    }
+
+    // Function untuk melihat data kantor ini dimiliki oleh kota yang mana makanya menggunakan belongsTo
+    public function city(): BelongsTo
+    {
+        return $this->belongsTo(City::class);
+    }
+
+    public function photos(): HasMany
+    {
+        return $this->hasMany(OfficeSpacePhoto::class);
+    }
+
+    public function benefits(): HasMany
+    {
+        return $this->hasMany(OfficeSpaceBenefit::class);
+    }
 }
