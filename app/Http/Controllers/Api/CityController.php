335206267c5e5da53927c7c1523f47e9c2f13cc4
TedoHarisC <tedoharischandra29@gmail.com>
2025-04-18 18:58:13 +0700

    Membuat controller setiap endpoint (City & OfficeSpace)

--- app/Http/Controllers/Api/CityController.php
+++ app/Http/Controllers/Api/CityController.php
@@ -3,9 +3,31 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\CityResource;
+use App\Models\City;
 use Illuminate\Http\Request;
 
 class CityController extends Controller
 {
     //
+    public function index()
+    {
+        $cities = City::withCount('officeSpaces')->get();
+        return CityResource::collection($cities);
+    }
+
+    /**
+     * Show a single city.
+     *
+     * @authenticated
+     * @responseFile responses/city.show.json
+     * @param  \App\Models\City  $city
+     * @return \Illuminate\Http\Response
+     */
+    public function show(City $city) // model binding (dan untuk detail salah satu kota nya)
+    {
+        $city->load('officeSpaces.city', 'officeSpaces.photos');
+        $city->loadCount('officeSpaces');
+        return new CityResource($city);
+    }
 }
--- app/Http/Controllers/Api/OfficeSpaceController.php
+++ app/Http/Controllers/Api/OfficeSpaceController.php
@@ -3,9 +3,29 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\OfficeSpaceResource;
+use App\Models\OfficeSpace;
 use Illuminate\Http\Request;
 
 class OfficeSpaceController extends Controller
 {
-    //
+    public function index()
+    {
+        $officeSpaces = OfficeSpace::withCount('officeSpaces')->get();
+        return OfficeSpaceResource::collection($officeSpaces);
+    }
+
+    /**
+     * Show a single city.
+     *
+     * @authenticated
+     * @responseFile responses/city.show.json
+     * @param  \App\Models\City  $city
+     * @return \Illuminate\Http\Response
+     */
+    public function show(OfficeSpace $officeSpace) // model binding (dan untuk detail salah satu kota nya)
+    {
+        $officeSpace->load(['city', 'photos', 'benefits']);
+        return new officeSpaceResource($officeSpace);
+    }
 }
